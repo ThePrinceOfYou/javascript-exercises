eea2bd28d38a0818e0ecb41994c179552d646791
Princeton <tonvalera@gmail.com>
2023-05-02 01:14:04 +0800

    Add solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+function sumAll(x, y) {
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (x < y) {
+        for (let i = x; i <= y; i++) {
+            sum += i;
+        }
+    } else if (x > y) {
+        for (let i = y; i <= x; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
