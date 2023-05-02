42708c9602e9898e829b9ab1ba95763897d0638e
Princeton <tonvalera@gmail.com>
2023-05-02 14:06:49 +0800

    Add solved exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+function convertToCelsius(fah) {
+  let cel = (fah - 32) * 5 / 9;
+  return Math.round(cel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+function convertToFahrenheit(cel) {
+  let fah = (cel * 9 / 5) + 32;
+  return Math.round(fah * 10) / 10;
 };
 
 // Do not edit below this line
