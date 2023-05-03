fae95daf592860e4e7240b506d9b16e16cf22add
Princeton <tonvalera@gmail.com>
2023-05-03 16:20:18 +0800

    Add solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+function palindromes(str) {
+    let format = /^[!@#$%^&*()_+\-=\[\]{} ;':"\\|,.<>\/?]*$/;
+    str = str.toLowerCase();
+    strNoSpecialCharacter = "";
+    for (let i = 0; i < str.length; i++) {
+        if (!(str[i].match(format))) {
+            strNoSpecialCharacter += str[i];
+        }
+    }
 
+    let strReversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (!(str[i].match(format))) {
+            strReversed += str[i];
+        }
+    }
+
+    return (strNoSpecialCharacter == strReversed);
 };
 
 // Do not edit below this line
