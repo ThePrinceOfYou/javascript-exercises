8fec2d95c085c73f418a3910bbb81fbd08b4e875
Princeton <tonvalera@gmail.com>
2023-05-03 17:11:39 +0800

    Add soved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+function fibonacci(input) {
+    if (input < 0) {
+        return "OOPS";
+    } else if (isNaN(input)) {
+        return "DOES accept strings";
+    }
 
+    let array = [1, 1];
+    for (let i = array.length; i < input; i++) {
+        array.push(array[array.length - 1] + array[array.length - 2]);
+    }
+
+    return array.pop();
 };
 
 // Do not edit below this line
