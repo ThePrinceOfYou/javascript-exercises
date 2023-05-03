17bad8995844d9adf7337fd5521a1a7d797a67c6
Princeton <tonvalera@gmail.com>
2023-05-03 17:39:54 +0800

    Add solved exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+function getTheTitles (array) {
+    return array.map((book) => book.title);
+    //array.map(function(currentValue, index, arr), thisValue)
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
